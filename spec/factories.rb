669d9702fceec2acbe25726f349688e346276373
Crystal Wong Kruger <crystalwongkruger@gmail.com>
2013-02-05 14:46:27 -0500

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do 
+	factory :user do
+		name	"Crystal Wong Kruger"
+		email	"crystalwongkruger@gmail.com"
+		password				"skule0t3"
+		password_confirmation	"skule0t3"
+	end
+end
